b2cab5a807cb49e748a1206f18ef4916dd5a30bf
venkat gadiyakari <venkatgadiyakari@venkats-MacBook-Air.local>
2025-01-28 23:05:22 +0530

    building the bookings table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,10 +4,15 @@ import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const { isLoading, error, cabins } = useCabins();
   const [searchParams] = useSearchParams();
+  if (!cabins.length) {
+    return <Empty resourceName="cabins" />;
+  }
+
   if (isLoading) {
     return <Spinner />;
   }
