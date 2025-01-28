6d622b49d79c3e49f28837df051be6e56e20e831
venkat gadiyakari <venkatgadiyakari@venkats-MacBook-Air.local>
2025-01-28 22:40:28 +0530

    Client-Side Sorting: Sorting Cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -12,6 +12,7 @@ function CabinTable() {
     return <Spinner />;
   }
   const filter = searchParams.get("discount") || "all";
+  //1.filter
   let filteredCabins;
   if (filter === "all") {
     filteredCabins = cabins;
@@ -22,6 +23,15 @@ function CabinTable() {
   if (filter === "with-discount") {
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
   }
+
+  //2.sort
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -34,7 +44,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
