101ff57e1a4ae5d493704f34ea90bf88d400c9c4
venkat gadiyakari <venkatgadiyakari@venkats-MacBook-Air.local>
2025-01-29 22:07:20 +0530

    API-Side Sorting: Sorting Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -9,13 +9,18 @@ export function useBookings() {
     !filteredValue || filteredValue === "all"
       ? null
       : { field: "status", value: filteredValue };
+
+  //SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { isLoading, error, bookings };
 }
