8089b6a48b7a9039a187976fbff9b68e7b7397b9
venkat gadiyakari <venkatgadiyakari@venkats-MacBook-Air.local>
2025-02-03 22:06:24 +0530

    implemented dark mode

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -83,7 +83,7 @@ export async function updateCurrentUser({ password, fullName, avatar }) {
     throw new Error(error.message);
   }
   //3. update avatar in the user
-  const { data: updatedUser, error: error2 } = supabase.auth.updateUser({
+  const { data: updatedUser, error: error2 } = await supabase.auth.updateUser({
     data: {
       avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
     },
