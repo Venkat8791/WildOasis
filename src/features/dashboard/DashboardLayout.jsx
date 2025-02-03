f0c45289a5d194cf65375c5813cc49e07d7aa442
venkat gadiyakari <venkatgadiyakari@venkats-MacBook-Air.local>
2025-02-03 22:15:15 +0530

    building the dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Todays Activity</div>
+      <div>Chart Stay Duration</div>
+      <div>Chart Sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
