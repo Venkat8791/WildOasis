dac45c34386718f3559d77838898958e9433fcd9
venkat gadiyakari <venkatgadiyakari@venkats-MacBook-Air.local>
2025-02-03 23:27:10 +0530

    adding a line chart for sales

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import Spinner from "../../ui/Spinner";
 import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -37,7 +38,7 @@ function DashboardLayout() {
       <div>Statistics</div>
       <div>Todays Activity</div>
       <div>Chart Stay Duration</div>
-      <div>Chart Sales</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
