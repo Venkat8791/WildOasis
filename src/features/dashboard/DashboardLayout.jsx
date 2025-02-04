32575db9de2bcf7a5e2cb2abc902488c31a4edb0
venkat gadiyakari <venkatgadiyakari@venkats-MacBook-Air.local>
2025-02-04 22:11:31 +0530

    added todays activity

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -17,7 +18,6 @@ const StyledDashboardLayout = styled.div`
 function DashboardLayout() {
   const { bookings, isLoading: isBookingsLoading } = useRecentBookings();
   const {
-    stays,
     confirmedStays,
     isLoading: isStaysLoading,
     numDays,
@@ -36,8 +36,7 @@ function DashboardLayout() {
         cabinCount={cabins.length}
         numDays={numDays}
       />
-      <div>Statistics</div>
-      <div>Todays Activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
